b332f24edf5f627ec7bd16880d8e2eeab6e6941c
314otrek <piotr.98zawadzki@gmailcom>
2025-06-25 15:34:00 +0200

    create drag and drop

--- image-renamer/src/App.js
+++ image-renamer/src/App.js
@@ -6,6 +6,7 @@ function App() {
   const [selectedFiles, setSelectedFiles] = useState([]);
   const [prefix, setPrefix] = useState('');
   const [suffix, setSuffix] = useState('');
+  const [isDragging, setIsDragging] = useState(false);
   const fileInputRef = useRef(null);
 
   const handleFileSelection = (event) => {
@@ -59,26 +60,53 @@ function App() {
     link.click();
   };
 
+  const handleDragOver = (event) => {
+    event.preventDefault();
+    setIsDragging(true);
+  };
+
+  const handleDragLeave = (e) => {
+    e.preventDefault();
+    setIsDragging(false);
+  };
+
+  const handleDrop = (event) => {
+    event.preventDefault();
+    setIsDragging(false);
+
+    const files = Array.from(event.dataTransfer.files);
+    const imageFiles = files.filter(file => file.type.startsWith('image/'));
+    setSelectedFiles(prev => [...prev, ...imageFiles]);
+  };
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-100 to-purple-200">
-      <div className="bg-white shadow-lg rounded-xl p-10 w-full max-w-md flex flex-col items-center">
-        <h1 className="text-3xl font-bold mb-4 text-purple-700 text-center">Change Photos Name</h1>
-        <p className="text-lg text-gray-600 mb-8 text-center">Click the button below to upload your photos.</p>
-       
-       <input
-          ref={fileInputRef}
-          type="file"
-          multiple
-          accept="image/*"
-          onChange={handleFileSelection}
-          className="hidden"
-        />
+  <div className="bg-gray-100 shadow-lg rounded-xl p-10 w-full max-w-2xl flex flex-col items-center">
+    <h1 className="text-3xl font-bold mb-6 text-gray-800 text-center">Change Photos Name</h1>
+   
+    <input
+      ref={fileInputRef}
+      type="file"
+      multiple
+      accept="image/*"
+      onChange={handleFileSelection}
+      className="hidden"
+    />
 
-        <button
-          onClick={openFileDialog}
-          className="bg-purple-600 text-white px-6 py-3 rounded-lg shadow">
-            Upload Photos
-            </button>
+    <div 
+      className={`w-full border-2 border-dashed rounded-lg p-12 text-center transition-colors cursor-pointer ${
+      isDragging ? 'border-blue-500 bg-blue-500' : 'border-gray-300'}`}
+      onDragOver={handleDragOver}
+      onDragLeave={handleDragLeave}
+      onDrop={handleDrop}
+      onClick={openFileDialog}
+    >
+      <p className="text-lg text-gray-600 mb-4">Click or drop images here</p>
+      <div className="bg-blue-700 text-white px-6 py-3 rounded-lg shadow inline-block">
+        Choose photos
+      </div>
+    </div>
+            
 
         <div className="flex items-center w-full mt-4">
           <label className="text-sm font-medium text-gray-700 mr-2 w-20">Prefix:</label>
@@ -102,33 +130,31 @@ function App() {
         </div>
       
         {selectedFiles.length > 0 && (
-          <div className="mt-6 w-full">
-            <h3>Look on new names</h3>
+          <div className="mt-6 w-full text-center py-4">
+            <h3>Preview of new image titles:</h3>
             {selectedFiles.map((file, index) => (
-              <div key={index}>
+              <div key={index} className='mt-2'>
                 {createNewFileName(file.name)}
               </div>
             ))}
       </div>
         )}
-        <div className="mt-6 w-full flex space-x-2">
-        <button
-          onClick={downloadAllFiles}
-          className="mt-6 bg-blue-600 text-white px-6 py-3 rounded-lg shadow">
-            Download All File
-        </button>
+        <div className="mt-6 w-full  space-x-2 flex items-center justify-center">
+      
         <button
           onClick={downloadZip}
-          className="mt-6 bg-blue-400 text-white px-6 py-3 rounded-lg shadow">
+          className="mt-6 bg-blue-700 text-white px-6 py-3 rounded-lg shadow">
             Download Zip
         </button>
         </div>
         <br/>
-        <button
-          onClick={clearAllFiles}
-          className="mt-4 bg-red-600 text-white px-6 py-3 rounded-lg shadow">
-            Clear All
-        </button>
+        {selectedFiles.length > 0 && (
+          <button
+            onClick={clearAllFiles}
+            className="mt-4 bg-red-600 text-white px-6 py-3 rounded-lg shadow">
+              Clear All
+          </button>
+        )}
     </div>
   
   </div>
